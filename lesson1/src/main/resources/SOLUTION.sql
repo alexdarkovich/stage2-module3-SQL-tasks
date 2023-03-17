a752137db02ae52e3bde2e14e0226df3d80c8a54
AlexDark <alexdarkovich@mail.ru>
2023-03-17 19:20:19 +0300

    1-3 completed

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,12 +1,8 @@
-CREATE TABLE student (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45), birthday DATE NOT NULL, groupnumber INT NOT NULL);
-
-CREATE TABLE subject (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(250), description VARCHAR(255), grade int NOT NULL);
-
-CREATE TABLE paymenttype( id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45));
-
-CREATE TABLE payment( id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, type_id BIGINT NOT NULL, amount decimal NOT NULL, payment_date TIMESTAMP  NOT NULL, student_id BIGINT NOT NULL, foreign key (type_id) references paymenttype(id), foreign key (student_id) references student(id));
-
-CREATE TABLE mark( id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT NOT NULL, foreign key (student_id) references student(id), foreign key (subject_id) references subject(id));
+CREATE TABLE Student (id BIGINT PRIMARY KEY, name VARCHAR(255), birthday DATE, group_num INT);
+CREATE TABLE Subject (id BIGINT PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), grade int);
+CREATE TABLE PaymentType(id BIGINT PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE Payment(id BIGINT PRIMARY KEY, type_id BIGINT, amount DECIMAL, student_id BIGINT, payment_date DATETIME, foreign key (type_id) REFERENCES PaymentType(id), foreign key (student_id) REFERENCES Student(id));
+CREATE TABLE Mark(id BIGINT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT, foreign key (student_id) REFERENCES Student(id), foreign key (subject_id) REFERENCES Subject(id));
 
 
 
